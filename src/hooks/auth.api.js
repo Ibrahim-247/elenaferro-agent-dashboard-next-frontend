df5f0da7bf7f3438c8b97c9244a895d999fd2e8b
karmakarCoder <dev.rodro@gmail.com>
2026-01-08 16:37:04 +0600

    login feature implement

---
+++ src/hooks/auth.api.js
@@ -0,0 +1,37 @@
+import { toast } from "sonner";
+import useApiMutation from "./useApiMutation";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { setAuth } from "@/redux/slices/authSlice";
+
+// login api
+export const useLogin = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  return useApiMutation({
+    key: "login",
+    method: "post",
+    endpoint: "/login",
+    onSuccess: (data, variables) => {
+      const remember = variables?.remember || false;
+      dispatch(
+        setAuth({
+          user: data?.data?.user,
+          token: data?.data?.token?.original?.access_token,
+          remember,
+        })
+      );
+      router.push("/");
+    },
+    onError: (error) => {
+      if (error?.code === "ERR_NETWORK") {
+        toast.error("Please check your network connection");
+      } else {
+        toast.error(error?.response?.data?.message || "Something went wrong!");
+      }
+
+      console.error("Login error:", error);
+    },
+  });
+};
