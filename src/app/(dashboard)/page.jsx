e1a345c239f616d563e2a4e40735c2800f65d906
karmakarCoder <dev.rodro@gmail.com>
2026-02-17 17:25:54 +0600

    dashboard analytics api implement

--- src/app/(dashboard)/page.jsx
+++ src/app/(dashboard)/page.jsx
@@ -1,12 +1,10 @@
-import DashboardStats from "@/components/DashboardComponents/DashboardStats";
-import DealsBarchart from "@/components/DashboardComponents/DealsBarchart";
+import DashboardMain from "@/components/DashboardComponents/DashboardMain";
 import RecentActivity from "@/components/DashboardComponents/RecentActivity";
 
 export default function page() {
   return (
     <div className="space-y-6">
-      <DashboardStats />
-      <DealsBarchart />
+      <DashboardMain />
       <RecentActivity />
     </div>
   );
